1777da509255023cda9114a6fca3f1c8b1cde993
Jörg Thalheim <joerg@thalheim.io>
2025-12-16 14:24:07 +0100

    switch to nixfmt

--- flake.nix
+++ flake.nix
@@ -7,32 +7,46 @@
     flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachSystem ["x86_64-linux"] (
-      system: let
-        nixpkgs' = nixpkgs.legacyPackages.${system};
-        pkgs = import self {
-          inherit system;
-          inputs = null;
-          nixpkgs = nixpkgs';
-        };
-      in {
-        defaultPackage = pkgs.default;
-        packages = pkgs;
-        devShells.default = pkgs.devShell;
-        checks = {
-          fmt = with nixpkgs';
-            runCommandLocal "fmt" {} ''
-              export HOME=$(mktemp -d)
-              cd ${./.}
-              ${treefmt}/bin/treefmt --fail-on-change > $out
-            '';
-        };
-      }
-    );
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachSystem
+      [ "x86_64-linux" ]
+      (
+        system:
+        let
+          nixpkgs' = nixpkgs.legacyPackages.${system};
+          pkgs = import self {
+            inherit system;
+            inputs = null;
+            nixpkgs = nixpkgs';
+          };
+        in
+        {
+          defaultPackage = pkgs.default;
+          packages = pkgs;
+          devShells.default = pkgs.devShell;
+          checks = {
+            fmt =
+              with nixpkgs';
+              runCommandLocal "fmt" { } ''
+                export HOME=$(mktemp -d)
+                cp -r ${./.} src
+                chmod -R u+w src
+                cd src
+                export PATH=${
+                  lib.makeBinPath [
+                    nixfmt-rfc-style
+                    gofumpt
+                  ]
+                }:$PATH
+                ${treefmt}/bin/treefmt --fail-on-change > $out
+              '';
+          };
+        }
+      );
 }
